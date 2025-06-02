764fb9366ee6084be115598911a07221c1b874ff
João Victor Tavares <168034029+Joaotavaresx@users.noreply.github.com>
2025-06-02 18:21:44 -0300

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from menu_principal import menu
-
-if __name__ == "__main__":
-    """
-    Ponto de entrada principal do sistema.
-    Executa a função menu() quando o script é chamado diretamente.
-    """
-    menu() # Inicia o sistema chamando o menu principal
